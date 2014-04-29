d57645792eb1fe221de3cb43c87af2f57fbb36a8
Odie <Odie@SBFCFBPC>
2014-04-29 21:08:41 +0100

    Created onlne repo for mvnPongServer. Created batch scripts to run a game

---
+++ startClient.bat
@@ -0,0 +1,3 @@
+cd Mvn
+cd MvnPong
+java -cp target/mvnPong-1.0-SNAPSHOT.jar om37.Client
---
+++ startServer.bat
@@ -0,0 +1,3 @@
+cd Mvn
+cd MvnPongServer
+java -cp target/java -cp target/mvnPongServer-1.0-SNAPSHOT.jar om37.Server
